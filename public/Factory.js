858612d24b38e8b6f4644fb383db7239be843a54
Bryan Stevens <bryanstevens@Bryans-MacBook-Pro.local>
2020-05-05 21:17:56 -0700

    Support - Bootstrap 4, updated styling

--- public/Factory.js
+++ public/Factory.js
@@ -3,31 +3,19 @@ const Factory = function(floor){
     return this
 }
 
-Factory.prototype.construct = function(data){
+Factory.prototype.construct = async function(data){
     this.floor.empty()
     const length = data.payload.length
     for(let x = 0; x < length; x++){
         if(data.type === 'projects'){
-            var project = new Project(data.payload[x]).build()
-            this.floor.append(project)
+            new Project(template=>{ this.floor.append(template({item: data.payload[x]})) })
         }
     }
     return this
 }
 
-const Project = function(item){
-    this.bluePrint = `         
-        <div id="template" class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
-                <p class="card-text">${item.description}</p>
-                <a href="${item.href}" target="_blank" class="card-link">Card link</a>
-            </div>
-        </div>
-    `
-    return this
-}
-
-Project.prototype.build = function(){
-    return $(this.bluePrint)
+const Project = function(callback){ 
+    $.get('templates/project.hbs', function (data) { 
+        callback(Handlebars.compile($(data).html())) 
+    }, 'html')
 }
